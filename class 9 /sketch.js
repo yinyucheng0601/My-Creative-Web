3121a3209375900d026c7a25f1b7b401a45420ee
yinyucheng0601 <42951346+yinyucheng0601@users.noreply.github.com>
2019-12-10 22:40:11 -0500

    colorpicker

--- class 9 /sketch.js
+++ class 9 /sketch.js
@@ -112,11 +112,39 @@ function draw() {
 	}
 }
 
+function rgbToHsv(r, g, b) {
+	r /= 255, g /= 255, b /= 255;
+  
+	var max = Math.max(r, g, b), min = Math.min(r, g, b);
+	var h, s, v = max;
+  
+	var d = max - min;
+	s = max == 0 ? 0 : d / max;
+  
+	if (max == min) {
+	  h = 0; // achromatic
+	} else {
+	  switch (max) {
+		case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+		case g: h = (b - r) / d + 2; break;
+		case b: h = (r - g) / d + 4; break;
+	  }
+  
+	  h /= 6;
+	}
+  
+	return [ 100*h, 50*s, 100 , 60 ];
+  }
+
 function handleDraw({ totalFoundPixels, sumX, sumY, objectR, objectB, objectG }) {
 	// average location of pixels
 	aveX = sumX / totalFoundPixels;
 	aveY = sumY / totalFoundPixels;
-	fill(objectR,objectG,objectB, 60);
+	rgbToHsv(objectR,objectG,objectB);
+	console.log(rgbToHsv(objectR,objectG,objectB));
+	colorMode(HSB,60);
+	//fill(objectR,objectG+10,objectB+10, Math.random() * 80);
+	fill(rgbToHsv(objectR,objectG,objectB));
 	noStroke()
 	var r = Math.random()*50;
 	ellipse(width-2*aveX,2*aveY, r, r);	
